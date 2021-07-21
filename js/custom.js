e230bbdf6fc2733efc0a1b230794f6ab37cf7294
Milo Vrach <morelo87@gmail.com>
2021-07-21 22:52:10 +0200

    Add a biografje page

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,8 @@
-
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".navbar");
+  //   const logo = document.querySelector(".logo");
+  //   const togle = document.querySelector(".tgl-switch");
+  header.classList.toggle("black", window.scrollY > 400);
+  //   logo.classList.toggle("black-logo", window.scrollY > 400);
+  //   togle.classList.toggle("black-logo", window.scrollY > 400);
+});
