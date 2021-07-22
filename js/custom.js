85ef4b0e825ee0bb6441818bd8f6f0465e772e72
Milo Vrach <morelo87@gmail.com>
2021-07-22 22:45:42 +0200

    Finish about us page

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,31 @@ window.addEventListener("scroll", function () {
   //   logo.classList.toggle("black-logo", window.scrollY > 400);
   //   togle.classList.toggle("black-logo", window.scrollY > 400);
 });
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 40,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      767: {
+        items: 2,
+        nav: false,
+      },
+      992: {
+        items: 2,
+        nav: true,
+        loop: false,
+      },
+      1200: {
+        items: 3,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
+});
