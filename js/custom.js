74a7f94bbae0825daf95ec7e07ca752103d75d85
Milo Vrach <morelo87@gmail.com>
2021-07-25 23:06:06 +0200

    Finish landing page

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,59 @@
-window.addEventListener("scroll", function () {
-  const header = document.querySelector(".navbar");
-  //   const logo = document.querySelector(".logo");
-  //   const togle = document.querySelector(".tgl-switch");
-  header.classList.toggle("black", window.scrollY > 400);
-  //   logo.classList.toggle("black-logo", window.scrollY > 400);
-  //   togle.classList.toggle("black-logo", window.scrollY > 400);
+//Select element function
+
+const selectElement = function (element) {
+  return document.querySelector(element);
+};
+
+let menuToggler = selectElement(".tgl-switch");
+let body = selectElement("body");
+let nav = selectElement(".nav-list");
+
+menuToggler.addEventListener("click", function () {
+  body.classList.toggle("open");
+});
+nav.addEventListener("click", function () {
+  body.classList.toggle("open");
 });
 
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".navbar");
+  header.classList.toggle("black", window.scrollY > 400);
+});
+
+// scroll reveal start
+window.sr = ScrollReveal();
+sr.reveal("#img-1", {
+  origin: "left",
+  duration: 1500,
+  delay: 50,
+  distance: "10px",
+});
+sr.reveal("#img-3", {
+  origin: "right",
+  duration: 1500,
+  delay: 50,
+  distance: "10px",
+});
+sr.reveal("#img-2", {
+  origin: "bottom",
+  duration: 1500,
+  delay: 40,
+  distance: "10px",
+});
+sr.reveal(".logoi", {
+  origin: "bottom",
+  duration: 1500,
+  delay: 30,
+  distance: "10px",
+});
+sr.reveal(".kontakt-div", {
+  origin: "bottom",
+  duration: 1500,
+  delay: 150,
+  distance: "10px",
+});
+
+// scroll reveal end
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
