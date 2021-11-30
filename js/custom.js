9036c07e78557ad11de83aaa2f473f5bead0fe64
Milo Vrach <morelo87@gmail.com>
2021-11-30 12:40:39 +0100

    Big ONE

--- js/custom.js
+++ js/custom.js
@@ -60,12 +60,12 @@ sr.reveal(".scroll-sm", {
   delay: 150,
   distance: "10px",
 });
-sr.reveal(".grid-item", {
-  origin: "bottom",
-  duration: 1500,
-  delay: 150,
-  distance: "20px",
-});
+// sr.reveal(".grid-item", {
+//   origin: "bottom",
+//   duration: 1500,
+//   delay: 150,
+//   distance: "20px",
+// });
 
 // scroll reveal end
 
@@ -106,3 +106,49 @@ $(document).ready(function () {
     },
   });
 });
+
+// modal
+
+function closeModal() {
+  document.getElementById("Modal").style.display = "none";
+}
+function openModal() {
+  document.getElementById("Modal").style.display = "block";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const grids = document.getElementsByClassName("grid-item");
+  const slikar = document.getElementById("slikar");
+  const imeSlike = document.getElementById("ime-slike");
+  const tehnika = document.getElementById("tehnika");
+  const dimenzije = document.getElementById("dimenzije");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+  slikar.innerHTML = slides[slideIndex - 1].title;
+  const arr = grids[slideIndex - 1].innerText.split("\n");
+  imeSlike.innerHTML = arr[0];
+  tehnika.innerHTML = arr[1].split(",")[0];
+  dimenzije.innerHTML = arr[1].split(",")[1];
+}
